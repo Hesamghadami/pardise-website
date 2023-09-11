5372dcd061825d1de9fc5afc3be812c315b509d8
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-09-11 17:52:29 +0330

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,7 @@ class address(models.Model):
     reciever = models.CharField(max_length=255, null= True, blank = True)
     phone = models.CharField(max_length=11, null= True, blank = True)
 
+
     def __str__(self):
         return self.name
 
@@ -23,7 +24,9 @@ class CustumUser(AbstractUser):
     birthdata = models.DateField(null=True, blank=True)
     melicode = models.CharField(max_length=10, null= True, blank = True)
     credit_number = models.CharField(max_length=16, null= True, blank=True)
-
+    is_custumUser = models.BooleanField(null=True, blank= True) 
     def __str__(self):
         return self.username
-# Create your models here.
+    
+
+
